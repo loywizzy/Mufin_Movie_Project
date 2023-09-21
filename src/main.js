e9ac6a7d4bf45ba78e5d3d7e05f4c663640f4d21
loywizzy <kdftvguitarandarmy@gmail.com>
2023-09-22 01:25:16 +0700

    version_0.1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(bootstrap)
 app.mount('#app')
